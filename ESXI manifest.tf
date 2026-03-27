d0d531f5a9c14a21d655deb2e7f7f39e9b2cd2d9
IngmarRaw <ingmarrenses@Hotmail.com>
2026-03-27 11:11:28 +0100

    Fix terraform file name

--- ESXI manifest.tf
+++ ESXI manifest.tf
@@ -13,11 +13,25 @@ provider "esxi" {
   esxi_username      = "root"
   esxi_password      = "Welkom01!"
 }
-resource "esxi_guest" "vmtest" {
-  guest_name         = "vmtest"
+resource "esxi_guest" "webserver" {
+  count              = "2"
+  guest_name         = "webserver-${count.index + 1}"
   disk_store         = "datastore1"
 
-  memsize            = "1024"
+  memsize            = "2048"
+  numvcpus           = "1"
+  power              = "on"
+
+  ovf_source = "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.ova"
+  network_interfaces {
+    virtual_network = "VM Network"
+  }
+}
+resource "esxi_guest" "databaseserver" {
+  guest_name         = "databaseserver"
+  disk_store         = "datastore1"
+
+  memsize            = "2048"
   numvcpus           = "1"
   power              = "on"
 
